37049177e7a66b0ef65653272f8b4ff4a3685a19
Om <sharadjain21@gmail.com>
2021-11-18 18:38:01 +0530

    updated message for a check

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ var os = require('os')
 var http = require('http')
 
 function handleRequest(req, res) {
-  res.write('Hi there!! I\'m being served from ' + os.hostname())
+  //res.write('Hi there!! I\'m being served from ' + os.hostname())
+  res.write('Chinu')
   res.end()
 }
 
 http.createServer(handleRequest).listen(8081)
+
+
+
